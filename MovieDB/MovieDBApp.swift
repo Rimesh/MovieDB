fdc43ab553ce49bd61bea304f8a4dc6dc5a13e5e
Rimesh Jotaniya <rimesh.j@icloud.com>
2024-06-02 15:06:39 +0200

    Initial Commit

---
+++ MovieDB/MovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBApp.swift
+//  MovieDB
+//
+//  Created by Rimesh Jotaniya on 02/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
